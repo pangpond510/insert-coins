592fff792e25d7e870e88cb3a54cf083acb2b586
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-09-10 10:26:33 +0700

    fix a bug

--- src/components/CompareView/compareView.js
+++ src/components/CompareView/compareView.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Row, Col, Card, Input } from "antd";
 import Button from "../Button";
-import compareGraph from "../../img/Compare1.png"
+import compareGraph from "../../img/Compare1.png";
 import Text from "../Text";
 
 import { COLOR } from "../../styles/variables";
@@ -15,10 +15,11 @@ const Container = styled.div`
 
 const SearchSection = styled.div`
   width: fit-content;
-  margin: 20px auto 40px;
+  padding: 50px 0px !important;
+  margin: 0px auto;
   display: block;
   vertical-align: middle;
-`
+`;
 const InfoCard = styled(Card)`
   box-shadow: 0 1.5px 9px rgba(0, 0, 0, 0.2);
   border-color: transparent;
@@ -74,21 +75,21 @@ class CompareView extends Component {
     array[name] = target.value;
 
     this.setState({
-      searchText: array,
+      searchText: array
     });
   }
 
   handleCompareClick() {
     this.setState({
-      isSearch: true,
-    })
+      isSearch: true
+    });
     for (let key in this.state.searchText) {
       if (this.state.searchText.hasOwnProperty(key)) {
         const island = this.state.searchText[key];
         const filter = {
           where: { island: island }
         };
-    
+
         this.state
           .apiGet(filter)
           .then(res => res.json())
@@ -100,54 +101,66 @@ class CompareView extends Component {
               count++;
             }
             const tmp = this.state.avgText;
-            tmp[key] = isNaN(sumPrice / count) ? 0 : sumPrice / count
+            tmp[key] = isNaN(sumPrice / count) ? 0 : sumPrice / count;
             this.setState({
-              avgText: tmp,
-            })
+              avgText: tmp
+            });
           })
           .catch(err => {
             console.log(err);
           });
-        }
       }
+    }
     console.log(this.state);
   }
 
   handleAddClick() {
     const count = this.state.searchBox + 1 > 4 ? 4 : this.state.searchBox + 1;
     this.setState({
-      searchBox: count,
-    })
+      searchBox: count
+    });
   }
 
   render() {
     return (
       <Container>
-        {
-          
-          !this.state.isSearch &&
+        {!this.state.isSearch && (
           <SearchSection>
-            {
-              [...Array(this.state.searchBox)].map((x,i) => (
-                <Input name={i} key = {i} value={this.state.searchText[i]} onChange={this.handleInputChange} placeholder={`island ${i+1} name...`} style={{ width: "70vw", height: 50, fontSize: 25, marginBottom: 20}}/>
-              ))
-            }
+            {[...Array(this.state.searchBox)].map((x, i) => (
+              <Input
+                name={i}
+                key={i}
+                value={this.state.searchText[i]}
+                onChange={this.handleInputChange}
+                placeholder={`island ${i + 1} name...`}
+                style={{ width: "70vw", height: 50, fontSize: 25, marginBottom: 20 }}
+              />
+            ))}
             <Row>
               <Col span={12}>
                 <Row type="flex" justify="center">
-                  <Button label=" Compare" icon="copy" size="20" onClick={this.handleCompareClick} />
+                  <Button
+                    label=" Compare"
+                    icon="copy"
+                    size="20"
+                    onClick={this.handleCompareClick}
+                  />
                 </Row>
               </Col>
               <Col span={11}>
                 <Row type="flex" justify="center">
-                  <Button label=" Add more island" icon="plus-circle" size="20" onClick={this.handleAddClick} />
+                  <Button
+                    label=" Add more island"
+                    icon="plus-circle"
+                    size="20"
+                    onClick={this.handleAddClick}
+                  />
                 </Row>
               </Col>
-              </Row>
+            </Row>
           </SearchSection>
-        }  
-        {
-          this.state.isSearch && 
+        )}
+        {this.state.isSearch && (
           <div>
             <br />
             <br />
@@ -155,41 +168,38 @@ class CompareView extends Component {
               <img src={compareGraph} alt="Compare Graph" width="100%" height="auto" />
             </Col>
             <Col span={9}>
-              {
-                Object.keys(this.state.searchText).map((key) => (
-                  <Row type="flex" justify="center" style={{ marginBottom: "20px" }}>
-                    <InfoCard style={{ width: "350" }}>
-                      <Card.Grid style={header}>
-                        <Text text="Average Price" size="24px" color={COLOR.green2} bold />
-                        <br />
-                        <Text
-                          text={`at ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}
-                          size="14px"
-                          color={COLOR.lightGrey4}
-                        />
-                      </Card.Grid>
-                      <Card.Grid style={content1}>
-                        <Text
-                          text={`Diesel price in ${this.state.searchText[key]}`}
-                          size="12px"
-                          color={COLOR.primaryDark}
-                        />
-                        <br />
-                        <Text
-                          text={`${Math.round(this.state.avgText[key] * 100) / 100} Baht/Litr`}
-                          size="20px"
-                          color={COLOR.peach}
-                          bold
-                        />
-                      </Card.Grid>
-                    </InfoCard>
-                  </Row>
-                ))
-              }
+              {Object.keys(this.state.searchText).map(key => (
+                <Row type="flex" justify="center" style={{ marginBottom: "20px" }}>
+                  <InfoCard style={{ width: "350" }}>
+                    <Card.Grid style={header}>
+                      <Text text="Average Price" size="24px" color={COLOR.green2} bold />
+                      <br />
+                      <Text
+                        text={`at ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}
+                        size="14px"
+                        color={COLOR.lightGrey4}
+                      />
+                    </Card.Grid>
+                    <Card.Grid style={content1}>
+                      <Text
+                        text={`Diesel price in ${this.state.searchText[key]}`}
+                        size="12px"
+                        color={COLOR.primaryDark}
+                      />
+                      <br />
+                      <Text
+                        text={`${Math.round(this.state.avgText[key] * 100) / 100} Baht/Litr`}
+                        size="20px"
+                        color={COLOR.peach}
+                        bold
+                      />
+                    </Card.Grid>
+                  </InfoCard>
+                </Row>
+              ))}
             </Col>
-            
           </div>
-        }
+        )}
       </Container>
     );
   }
